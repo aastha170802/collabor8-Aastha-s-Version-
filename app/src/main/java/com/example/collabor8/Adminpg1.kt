7737dfbc45ba69614fd9d65a218336cd60dc0830
aastha170802 <aasthapatil2002@gmail.com>
2022-09-17 00:02:44 +0530

    aastha's first commit

---
+++ app/src/main/java/com/example/collabor8/Adminpg1.kt
@@ -0,0 +1,11 @@
+package com.example.collabor8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Adminpg1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.adminpg1)
+    }
+}
